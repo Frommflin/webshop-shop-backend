990e5a3548db018c3963c0c7da0169e3b62f136d
Klara Frommelin <freakyfrommelin@outlook.com>
2025-10-24 11:21:26 +0200

    implementing mongodb-memory-server to test connection in CI pipeline

--- src/config/db.js
+++ src/config/db.js
@@ -1,13 +1,33 @@
+import { MongoMemoryServer } from "mongodb-memory-server";
 import mongoose from "mongoose";
+
+let mongoServer;
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_CONNECTION_STRING);
+    let mongoConString = process.env.MONGODB_CONNECTION_STRING;
+
+    if (!mongoConString) {
+      console.log(
+        "No MongoDB connection string found in .env, using MongoMemoryServer for CI/test"
+      );
+      mongoServer = await MongoMemoryServer.create();
+      mongoConString = mongoServer.getUri();
+    }
+
+    const conn = await mongoose.connect(mongoConString);
     console.log(`MongoDB connected: ${conn.connection.name}`);
   } catch (error) {
     console.error("MongoDB connection error:", error.message);
     process.exit(1);
   }
+};
+
+// Closing server, for CI testing
+export const closeDB = async () => {
+  await mongoose.disconnect();
+  if (mongoServer) await mongoServer.stop();
+  console.log("MongoMemoryServer stopped");
 };
 
 export default connectDB;
