23fe2afdec4ce863eb2e2cf8535bba02bfe00a13
rozabelay <rozaybeyn.belay@gmail.com>
2025-10-27 10:45:06 +0100

    created order model

--- src/models/orderModel.js
+++ src/models/orderModel.js
@@ -1,8 +1,11 @@
 import mongoose from 'mongoose';
 
 const orderSchema = new mongoose.Schema({
+
   userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
   userName: { type: String, required: true },
+
+
   items: [
     {
       productId: { type: mongoose.Schema.Types.ObjectId, ref: "Product", required: true },
@@ -14,8 +17,17 @@ const orderSchema = new mongoose.Schema({
       quantity: { type: Number, required: true },
     },
   ],
+
+
   totalAmount: { type: Number, required: true },
-  status: { type: String, default: "pending" },
-}, { timestamps: true });
+
+  status: { 
+    type: String, 
+    enum: ["pending", "processing", "shipped", "delivered", "cancelled"], 
+    default: "pending" 
+  },
+}, 
+{ timestamps: true } 
+);
 
 export default mongoose.model("Order", orderSchema);
